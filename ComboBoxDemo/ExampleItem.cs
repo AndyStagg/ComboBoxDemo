5af53d632278ee0dfd02fb7a098ece03e927302a
andy stagg <atwinda@gmail.com>
2021-07-29 11:04:13 -0700

    Add project files.

---
+++ ComboBoxDemo/ExampleItem.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Media;
+
+namespace ComboBoxDemo
+{
+    public class ExampleItem
+    {
+        public string DisplayText { get; set; }
+        public Brush Color { get; set; }
+    }
+}
